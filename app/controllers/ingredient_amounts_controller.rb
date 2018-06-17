bf0528a6cd8142a9752e561515986d91ea250e43
Kyle Hughes <blinded93@gmail.com>
2018-06-17 11:29:25 -0700

    add index and show actions to test ajax calls in postman.

--- app/controllers/ingredient_amounts_controller.rb
+++ app/controllers/ingredient_amounts_controller.rb
@@ -1,2 +1,7 @@
 class IngredientAmountsController < ApplicationController
+  def index
+    # binding.pry
+    ingredient_amount = IngredientAmount.for_id(params[:recipe_id])
+    render json: ingredient_amount, status: 200
+  end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,11 @@
 class RecipesController < ApplicationController
+  def index
+    recipes = Recipe.all
+    render json: recipes, status: 200
+  end
+
+  def show
+    recipe = Recipe.find(params[:id])
+    render json: recipe, status: 200
+  end
 end
