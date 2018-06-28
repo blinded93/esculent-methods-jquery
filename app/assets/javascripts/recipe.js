d155d54f2cc350fbe674fd2059c413c79f76232c
Kyle Hughes <blinded93@gmail.com>
2018-06-27 17:33:48 -0700

    change assignment to use dot notation. add abstracted constructor methods to display recipes.

--- app/assets/javascripts/recipe.js
+++ app/assets/javascripts/recipe.js
@@ -1,9 +1,42 @@
 function Recipe(json) {
+  this.assignIngredients = (ingredients) => {
+    const array = [];
+    if (ingredients) {
+      ingredients.forEach((ingredient) => {
+        array.push(ingredient);
+      });
+    }
+    return array;
+  };
+
   this.name = json.name;
   this.directions = json.directions;
-  this.cookTime = json['cook-time'];
-  this.prepTime = json['prep-time'];
+  this.cookTime = json.cook_time;
+  this.prepTime = json.prep_time;
   this.servings = json.servings;
-  this.skillLevel = json['skill-level'];
-  this.ingredients = [];
+  this.skillLevel = json.skill_level;
+  this.ingredients = this.assignIngredients(json.ingredient_amounts);
+  this.user = new User(json.user)
 }
+
+Recipe.createObjs = (recipes) => {
+  recipes.recipes.forEach((recipe) => {
+    recipeObjs.push(new Recipe(recipe));
+  });
+}
+
+Recipe.getJson = () => {
+  recipeObjs = [];
+  $.get("/recipes", Recipe.createObjs);
+  return recipeObjs;
+};
+
+Recipe.displayAll = (recipeObjs) => {
+  $("#mainContent")
+    .append(HandlebarsTemplates.recipe_results({recipes:recipeObjs}));
+};
+
+$(function() {
+  $("#esculentMethods").click(() => {})
+  Recipe.displayAll();
+})
