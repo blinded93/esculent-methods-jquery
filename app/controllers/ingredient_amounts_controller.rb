cbbac9b6ceeb510fb9c33cb617f456b0b292f3ca
Kyle Hughes <blinded93@gmail.com>
2018-07-01 19:02:09 -0700

    remove binding.pry

--- app/controllers/ingredient_amounts_controller.rb
+++ app/controllers/ingredient_amounts_controller.rb
@@ -1,6 +1,5 @@
 class IngredientAmountsController < ApplicationController
   def index
-    # binding.pry
     ingredient_amount = IngredientAmount.for_id(params[:recipe_id])
     render json: ingredient_amount, status: 200
   end
