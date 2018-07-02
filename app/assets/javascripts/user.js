8651976c97dac898cda82cf442c79d6cae06849f
Kyle Hughes <blinded93@gmail.com>
2018-07-01 18:50:27 -0700

    finish getRecipes() to retreive all of a users recipes, through a nested route, using the Display model to add them to the DOM.

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -2,4 +2,35 @@ function User(json) {
   this.username = json.username;
   this.email = json.email;
   this.id = json.id;
+  this.recipes = this.assignRecipes(json.recipes);
 }
+
+// User.linkListeners = function() {
+//   User.link();
+//   User.recipeLink();
+// };
+//
+// User.links = function() {
+//   $(".userLink").each((i, link) => {
+//     const $link = $(link);
+//     $link.click({link:$link}, (e) => {
+//       e.preventDefault();
+//       User.getRecipes($link.data("id"));
+//     });
+//   });
+// };
+
+User.prototype.assignRecipes = function(recipes) {
+  return recipes ? recipes.map(r => new Recipe(r)): [];
+};
+
+User.getRecipes = function(id) {
+  $.get(`/users/${id}/recipes`, owner => {
+    let objs = owner.recipes.map(recipe => {
+      return new Recipe(recipe);
+    });
+    Display.adjustBreacrumb();
+    Display.fromTemplate("recipe_results", {recipes:objs});
+    Display.linkListeners();
+  })
+};
