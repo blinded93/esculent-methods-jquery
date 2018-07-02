823aa1f22c43c28220f2dac476b8fe9e5525c9e9
Kyle Hughes <blinded93@gmail.com>
2018-07-01 19:00:51 -0700

    remove unneeded resources and nested recipes, all but index, under users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'home#index'
-  resources :ingredient_amounts
-  resources :ingredients
-  resources :recipes
-  resources :users
+  # resources :ingredient_amounts
+  # resources :ingredients
+  resources :recipes, only: [:index]
+  resources :users, except: [:index, :show] do
+    get '/recipes' => 'users#show'
+    resources :recipes, except: [:index, :new, :edit]
+  end
 end
