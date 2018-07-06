5181ecd7d0b48cbfa4980644643fa82ea2fc47a3
Kyle Hughes <blinded93@gmail.com>
2018-07-05 22:41:58 -0700

    remove eventListener/breadcrumb functionality to their respective objects.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -18,36 +18,24 @@ Display.templates = HandlebarsTemplates;
 
 Display.attachListeners = function() {
   setSearchForm();
-  $("#home").click(() => Recipe.getAllRecipes());
 };
 
 Display.fromTemplate = function(template, obj) {
-  let html = this.templates[template](obj);
-  $("#mainContent").hide().html(html).fadeIn("slow");
-}
-
-Display.linkListeners = function() {
-  // Display.links(".recipeLink", Recipe.get)
-  Display.links(".userLink", User.getRecipes)
+  const html = this.templates[template](obj);
+  $("#mainContent").hide().html(html).fadeIn(1000);
 };
 
-Display.links = function(linkClass, getFunc) {
-  $(linkClass).each((i, link) => {
-    const $link = $(link);
-    $link.click(e => {
-      e.preventDefault();
-      getFunc($link.data("id"));
-    });
+Display.homeListener = function() {
+  const $home = $("#home");
+  $home.addClass("linkCursor");
+  $home.click((e) => {
+    e.preventDefault();
+    $(".breadcrumb li:not(:first)").remove();
+    Recipe.getAllRecipes();
+    $home.removeClass("linkCursor");
+    $home.off("click");
   });
 };
-
-Display.adjustBreacrumb = function() {
-  $bc = $("#breadcrumbOl");
-
-  debugger;
-}
-
-
 
 // Menu
 
