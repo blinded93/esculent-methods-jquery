aabe7f02eef5efa0532c890b2864f51104382cca
Kyle Hughes <blinded93@gmail.com>
2018-07-21 20:13:38 -0700

    small style changes for development, before picutres are added.

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -5,35 +5,72 @@ function User(json) {
   this.recipes = this.assignRecipes(json.recipes);
 }
 
+User.displayAllUsers = function(data) {
+  const objs = data.users.map(user => new User(user));
+  Display.fromTemplate("users", {users:objs});
+  objs.forEach(user => {
+    user.resultListeners();
+  });
+};
+
 User.prototype.assignRecipes = function(recipes) {
   return recipes ? recipes.map(r => new Recipe(r)): [];
+};
+
+User.prototype.resultListeners = function() {
+  const parent = `#user-${this.id}`;
+  this.listener(parent);
+  this.favoritesListener(parent);
+  this.recipesListener(parent);
+};
+
+User.prototype.recipesListener = function(parentDiv) {
+
+};
+
+User.prototype.favoritesListener = function(parentDiv) {
+
 };
 
 User.prototype.listener = function(parentDiv) {
   const user = this;
   $(parentDiv).find(".userLink").click(function(e) {
     e.preventDefault();
-    user.getRecipes();
+    user.display();
   }).addClass("linkCursor");
+};
+
+User.prototype.profileListeners = function() {
+  this.listener(`#user-${this.id}`);
+};
+
+User.prototype.display = function() {
+  Display.fromTemplate("user", this);
+  this.adjustBreadcrumb();
+  this.profileListeners();
 };
 
 User.prototype.getRecipes = function() {
   const user = this;
   $.get(`/users/${user.id}/recipes`, data => {
-    let objs = data.user.recipes.map(recipe => new Recipe(recipe));
-    Display.fromTemplate("recipe_results", {recipes:objs});
-    objs.forEach(recipe => {
-      recipe.owner = user;
-      recipe.listener(`#recipe-${recipe.id}`);
-    });
-    user.adjustBreadcrumb();
+    user.displayRecipes(user, data);
   });
 };
 
+User.prototype.displayRecipes = function(user, data) {
+  let objs = data.user.recipes.map(recipe => new Recipe(recipe));
+  Display.fromTemplate("recipe_results", {recipes:objs});
+  objs.forEach(recipe => {
+    recipe.owner = user;
+    recipe.listener(`#recipe-${recipe.id}`);
+  });
+  user.adjustBreadcrumb();
+};
+
 User.prototype.adjustBreadcrumb = function() {
-  $bc = $(".breadcrumb");
+  const $bc = $(".breadcrumb");
   const $li = $("<li>", {"class": "breadcrumb-item userLink"}).html(this.username);
   Display.homeListener();
-  $bc.children(".userLink").remove();
+  Display.removeLastBreadcrumb();
   $bc.append($li);
 };
