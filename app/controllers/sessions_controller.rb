f542cdceda8071091e4c6d32ebb0a9819927ab6f
Kyle Hughes <blinded93@gmail.com>
2018-07-25 10:05:31 -0700

    add current_session_user to retrieve current_user for profile/menu purposes. create action for logging in using Session service object. destroy action for logging out.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,20 @@
 class SessionsController < ApplicationController
-  def new
+
+  def current_session_user
+    if current_user
+      render json: current_user, status: 200
+    end
   end
 
   def create
+    s = SessionService.create_and_authenticate(params)
+    s.set_current_user(session)
+    render json: s.result, status: 200
   end
 
-  def destroy    
+  def destroy
+    if current_user
+      session.delete(:user_id)
+    end
   end
 end
