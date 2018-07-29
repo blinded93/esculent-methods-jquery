bd44aa6479b8df02c4abb16bddcaff3a80bd28b8
Kyle Hughes <blinded93@gmail.com>
2018-07-29 14:09:27 -0700

    add conditional to index to display certain recipes depending on presence of params[:user_id]. add favorited/favorite get/post routes to get/change status of favorite by current_user.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,14 +1,37 @@
 class RecipesController < ApplicationController
   def index
-    recipes = Recipe.by_favorites
+    if params[:user_id]
+      recipes = Recipe.for(params[:user_id])
+    else
+      recipes = Recipe.by_favorites
+    end
+
     render json: recipes, status: 200
   end
 
   def show
-    recipe = Recipe.find(params[:id])
+    recipe = Recipe.find_by_id(params[:id])
     render json: recipe,
            serializer: RecipeIngredientsSerializer,
            include:['ingredient_amounts.ingredient'],
            status: 200
+  end
+
+  def favorited
+    favorite = FavoritedRecipe.find_by(recipe_id:params[:recipe_id],
+                                       user:current_user)
+    render json: {favorite: favorite.try(:id)},
+           status:200
+  end
+
+  def favorite
+    favorite = FavoritedRecipe.find_by(params[:favorite_id])
+    if !!favorite
+      favorite.destroy
+    else
+      favorite = FavoritedRecipe.create({recipe_id:params[:recipe_id],                                       user:current_user})
+    end
+    render json: {favorite: favorite.try(:id)},
+           status:200
   end
 end
