919b20c2af9b4214301b9dc793671d689f6dd284
Kyle Hughes <blinded93@gmail.com>
2018-07-29 14:12:00 -0700

    add create action for signup.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
   def create
-
+    user = User.new(user_params)
+    user.save
+    render json: user, status: 200
   end
-  
+
   def show
     user = User.find(params[:user_id])
     render json: user, status: 200
+  end
+
+  private
+  def user_params
+    params.permit(:username, :email, :password)
   end
 end
