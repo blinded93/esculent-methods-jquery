046dadad6d76646530f11c5b3a894447aa150ad8
Kyle Hughes <blinded93@gmail.com>
2018-08-27 00:21:36 -0700

    remove breadcrumb and listeners to respective files. add back to search results function.

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,60 +1,52 @@
 function Search() {
   this.form = $("#search");
+  this.type = $("#type option:selected");
   this.query = $("#query");
   this.submit = $("#submitSearch");
 }
 
 Search.setup = function() {
   const search = new Search();
-  search.setListener(search);
+  Listener.setSearch(search);
 };
 
-Search.prototype.setListener = function(search) {
-  this.submit.click(function(e) {
-    e.preventDefault();
-    search.listener(search);
-  });
-};
-
-Search.prototype.listener = function(search) {
-  $.post("/search", search.form.serialize())
-    .done(function(resp){
-      search.evaluateResp(resp);
-    })
-    .done(function(){
-      search.query.val("");
-    });
-};
-
-Search.prototype.adjustBreadcrumb = function() {
-  const $bc = $(".breadcrumb");
-  const $li = $("<li>", {"class": "breadcrumb-item searchLink"})
-                .html(`Search results for <span class="black">'${this.query.val()}'</span>...`);
-  Listener.setHome();
-  Display.removeLastBreadcrumb();
-  $bc.append($li);
+Search.backToResultsLink = function() {
+  if (!!$(".searchLink").length) {
+    Display.toggleAlert();
+    switchElementData(".searchLink", "#toSearchResults");
+    Listener.setBackToResults();
+  }
 };
 
 Search.prototype.displayErrors = function(resp) {
-  Display.fromTemplate("error", resp);
+  Display.fromTemplate("error", resp)
+    .toElement("#mainContent");
 };
 
 Search.prototype.evaluateResp = function(resp) {
   const search = this;
-  const bcTitle = `Search results for <span class="black">'${search.query.val()}'</span>...`
   switch (true) {
     case !!resp.search:
       search.errors = resp.search.errors;
       search.displayErrors(resp);
-      search.adjustBreadcrumb();
+      Listener.setHome();
+      Breadcrumb.reset();
       break;
     case !!resp.recipes:
-      Recipe.displayAllRecipes(resp);
-      search.adjustBreadcrumb();
+      Recipe.displayAllRecipes(resp, "recipes", "#mainContent");
+      Breadcrumb.search();
       break;
     case !!resp.users:
-      User.displayAllUsers(resp);
-      search.adjustBreadcrumb();
+      User.displayAllUsers(resp, "users", "#mainContent");
+      Breadcrumb.search();
       break;
   }
+  $(".searchLink").data({
+    type: search.type.val(),
+    query: search.query.val(),
+    page: 1,
+    search: search
+  });
+  Display.createSearchAlert(search.form.data("query"));
+  return this;
 };
