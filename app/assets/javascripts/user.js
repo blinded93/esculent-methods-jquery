0b7af8965614f80a51babee2384e299a4e19f94a
Kyle Hughes <blinded93@gmail.com>
2018-08-27 00:26:13 -0700

    change assignAssets functions to create model objects before assignment. allow displayAllUsers to account for empty array. allow getAssets to return assets for full page or a preview of results.

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -3,58 +3,91 @@ function User(json) {
   this.email = json.email;
   this.id = json.id;
   this.recipes = this.assignRecipes(json.recipes);
+  this.favorites = [];
+  this.friends = [];
 }
 
-User.displayAllUsers = function(data) {
-  const objs = data.users.map(user => new User(user));
-  Display.fromTemplate("users", {users:objs})
-    .done(function() {
-      objs.forEach(user => {
-        Listener.setUserResults(user);
-        // user.resultListeners();
-      });
-    });
+User.displayAllUsers = function(data, userType, destination) {
+  const usersJson = data[`${userType}`]
+  const users = User.createFrom(usersJson);
+  if (destination === "#mainContent") {
+    Breadcrumb.userAssets(data, "Friends");
+  }
+  if (isEmpty(users)) {
+    Display.nothingHere(destination);
+  } else {
+    Display.fromTemplate("users", {users:users})
+      .toElement(destination)
+        .done(function() {
+          Listener.setUserResults(users);
+        });
+  }
 };
 
-User.prototype.assignRecipes = function(recipes) {
-  return recipes ? recipes.map(r => new Recipe(r)): [];
+User.createFrom = function(data) {
+  return data ? data.map(user => new User(user)) : [];
+};
+
+User.prototype.assignRecipes = function(recipes, type='recipes') {
+  this[`${type}`] = recipes ? Recipe.createFrom(recipes) : [];
+};
+
+User.prototype.assignFriends = function(friends) {
+  this.friends = User.createFrom(friends);
 };
 
 User.prototype.displayProfile = function() {
   const user = this;
-  Display.fromTemplate("user", user)
-    .done(function() {
-      Breadcrumb.adjust(user.username, "userLink")
-      user.profileListeners();
+  $.get(`/users/${user.id}`)
+    .done(function(data) {
+      Display.fromTemplate("user", user)
+        .toElement("#mainContent")
+          .done(function() {
+            Breadcrumb.profile(user);
+            Listener.setProfile(user);
+          });
     });
 };
 
-User.prototype.getRecipes = function(user) {
-  $.get(`/users/${user.id}/recipes`, data => {
-    Breadcrumb.adjust("My Recipes", "recipeLink")
-    user.adjustBreadcrumb("My Recipes");
-    Recipe.displayAllRecipes(data);
-  });
+User.prototype.displayPreview = function(tab, type) {
+  const assets = this[`${tab.toLowerCase()}`];
+  Listener.setSeeAll(this, tab, type);
+  if (type === "recipes") {
+    Recipe.displayAllRecipes(this, tab.toLowerCase(), "#profileContent");
+  } else {
+    User.displayAllUsers(this, tab.toLowerCase(), "#profileContent");
+  }
 };
 
-User.prototype.getFavorites = function(user) {
-  $.get(`/users/${user.id}/favorites`, data => {
-    user.adjustBreadcrumb("My Favorites");
-    Recipe.displayAllRecipes(data);
-  });
+User.prototype.getRecipes = function(preview) {
+  const user = this;
+  const dfd = $.Deferred();
+  $.get(`/users/${user.id}/recipes`, {"preview":preview})
+    .done(function(data) {
+      const recipes = Recipe.createFrom(data.recipes);
+      dfd.resolve(recipes);
+    });
+    return dfd.promise();
 };
 
-User.prototype.getFriends = function(user) {
-  $.get(`/users/${user.id}/friends`, data => {
-    user.adjustBreadcrumb("Friends");
-    User.displayAllUsers(data);
-  });
+User.prototype.getFavorites = function(preview) {
+  const user = this;
+  const dfd = $.Deferred();
+  $.get(`/users/${user.id}/favorites`, {"preview":preview})
+    .done(function(data) {
+      const recipes = Recipe.createFrom(data.recipes);
+      dfd.resolve(recipes);
+    });
+    return dfd.promise();
 };
 
-User.prototype.adjustBreadcrumb = function(title) {
-  const $bc = $(".breadcrumb");
-  const $li = $("<li>", {"class": "breadcrumb-item userLink"}).html(title || this.username);
-  Listener.setHome();
-  Display.removeLastBreadcrumb();
-  $bc.append($li);
+User.prototype.getFriends = function(preview) {
+  const user = this;
+  const dfd = $.Deferred();
+  $.get(`/users/${user.id}/friends`, {"preview":preview})
+    .done(function(data) {
+      const friends = User.createFrom(data.friends);
+      dfd.resolve(friends);
+    });
+    return dfd.promise();
 };
