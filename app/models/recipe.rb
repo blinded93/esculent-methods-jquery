372cb471d002a854eb4b431068ea56f8470cd2cc
Kyle Hughes <blinded93@gmail.com>
2018-09-15 15:59:56 -0700

    add ingredient_attributes to find or create an ingredient and it's associated model.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,10 +2,12 @@ class Recipe < ApplicationRecord
   belongs_to :owner, class_name: "User", foreign_key: "user_id"
   has_many :favorited_recipes
   has_many :favoriters, through: :favorited_recipes, source: :user
-  has_many :ingredient_amounts
+  has_many :ingredient_amounts, dependent: :destroy
   has_many :ingredients, through: :ingredient_amounts
 
   serialize :directions, Array
+
+  mount_uploader :image, ImageUploader
 
   scope :by_favorites, -> {
     left_joins(:favorited_recipes)
@@ -20,6 +22,19 @@ class Recipe < ApplicationRecord
   scope :for, -> (user_id) { where(user_id: user_id) }
   scope :search, -> (query) { where("name like ?", "%#{query}%") }
   scope :preview, -> () { order(Arel.sql("random()")).limit(5) }
+
+  def ingredient_attributes=(ingredients)
+    ingredients.each do |ingredient|
+      i = Ingredient.find_or_create_by(name:ingredient[:name].capitalize)
+      self.ingredient_amounts.build.tap do |ia|
+        ia.quantity = ingredient[:quantity]
+        ia.unit = ingredient[:unit]
+        ia.state = ingredient[:state].include?("State") ? "" : ingredient[:state]
+        ia.ingredient = i
+        ia.save
+      end
+    end
+  end
 
   def self.with_ingredients(ingredient_ids)
     recipes = []
