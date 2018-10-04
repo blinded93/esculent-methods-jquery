2a170d8b99b255186f2b07d36e2f7afee4e6b719
Kyle Hughes <blinded93@gmail.com>
2018-10-03 18:14:37 -0700

    add routes for user, recipe and ingredient search.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   post '/signup' => 'users#create'
   delete '/logout' => 'sessions#destroy'
   get '/current_user' => 'sessions#current_session_user'
+  get '/recipe_search' => 'recipes#search'
+  get '/ingredient_search' => 'recipes#ingredient_search'
+  get '/user_search' => 'users#search'
   resources :recipes, only: [:index] do
     get '/favorited' => 'recipes#favorited'
     post '/favorite' => 'recipes#favorite'
