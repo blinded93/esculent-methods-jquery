0ff4a54d5bd1ea651d4d32be76da92bf35c1e483
Kyle Hughes <blinded93@gmail.com>
2018-10-03 18:12:51 -0700

    add functions to set paginate links.

--- app/assets/javascripts/paginate.js
+++ app/assets/javascripts/paginate.js
@@ -6,3 +6,27 @@ function Paginate(meta) {
   this.next = meta.next;
   this.query = meta.vars.query;
 }
+
+Paginate.prototype.setLinks = function(url, params) {
+  this.links = $("a.page-link");
+  const pageObj = this;
+  pageObj.links.each(function(i, link) {
+    pageObj.setLink(link, url, params);
+  });
+};
+
+Paginate.prototype.setLink = function(link, url, params) {
+  const page = $(link).data("page");
+  $(link).click(function(e) {
+    e.preventDefault();
+      if (!$(link).parent().is(".disabled", ".active")) {
+        $.get(url + `?page=${page}`, params)
+          .done(function(data) {
+            Recipe.displayAllRecipes(data, "recipes", "#mainContent")
+              .done(function(pageObj) {
+                pageObj.setLinks(url, params);
+              });
+          });
+      }
+  });
+};
