2eb65ffc07eef94cc7fa9c9415a6f71336023713
Kyle Hughes <blinded93@gmail.com>
2018-10-07 15:14:02 -0700

    add sent/recieved messages to user class.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,31 @@
 class User < ApplicationRecord
-  has_many :recipes
-  has_many :favorited_recipes
-  has_many :favorites, through: :favorited_recipes, source: :recipe
-  has_and_belongs_to_many :friends,
-    class_name: "User",
-    join_table: :friendships,
-    foreign_key: :user_id,
-    association_foreign_key: :friend_id
-
-  validates :email, :username, presence: true, uniqueness: true
+  validates :email, :username,
+      presence: true,
+      uniqueness: true
   has_secure_password
   mount_uploader :avatar, AvatarUploader
+
+  has_many :recipes
+  has_many :favorited_recipes
+  has_many :favorites,
+      through: :favorited_recipes,
+      source: :recipe
+  has_many :recieved_messages,
+      class_name: 'Message',
+      primary_key: 'id',
+      foreign_key: 'recipient_id',
+      dependent: :destroy
+  has_many :sent_messages,
+      class_name: 'Message',
+      primary_key: 'id',
+      foreign_key: 'sender_id',
+      dependent: :destroy
+  has_and_belongs_to_many :friends,
+      class_name: "User",
+      join_table: :friendships,
+      foreign_key: :user_id,
+      association_foreign_key: :friend_id
+
 
   scope :from_email, -> (email) { where("email like ?", "%#{email}%") }
   scope :from_username, -> (username) { where("username like ?", "%#{username}%") }
