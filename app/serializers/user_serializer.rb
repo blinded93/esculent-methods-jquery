06a9c8ead3ba764e8594c7b008dc384c68728566
Kyle Hughes <blinded93@gmail.com>
2018-10-07 15:13:15 -0700

    add friends to user serializer.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email, :avatar, :errors
+  attributes :id, :username, :email, :avatar, :friends, :errors
+
+  has_many :friends
 end
