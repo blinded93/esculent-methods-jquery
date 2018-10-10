6ca5960bae7d1126d81170d8661507c4f78b55c2
Kyle Hughes <blinded93@gmail.com>
2018-10-09 18:26:04 -0700

    add message templates and stubbed out messages index action.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,10 @@
 class MessagesController < ApplicationController
+  def index
+    user = User.find(params[:user_id])
+    binding.pry
+    pagy, messages = pagy(user.received_messages, {items:25})
+    render json: messages,
+           meta: pagy,
+           status: 200
+  end
 end
