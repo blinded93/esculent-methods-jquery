19be838dd71634bb9011a773b03c1f2ae0702cc1
Kyle Hughes <blinded93@gmail.com>
2018-10-17 16:16:49 -0700

    remove friends from user serializer.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email, :avatar, :friends, :errors
-
-  has_many :friends
+  attributes :id, :username, :email, :avatar, :errors
 end
