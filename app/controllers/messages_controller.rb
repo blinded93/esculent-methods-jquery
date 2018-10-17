f202d0eeb953659ea4b00d86632189d8bf1cb438
Kyle Hughes <blinded93@gmail.com>
2018-10-17 16:19:33 -0700

    add structure for create/destroy actions in message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,22 @@
 class MessagesController < ApplicationController
   def index
     user = User.find(params[:user_id])
-    binding.pry
     pagy, messages = pagy(user.received_messages, {items:25})
     render json: messages,
            meta: pagy,
+           status: 200
+  end
+
+  def create
+    binding.pry
+  end
+
+  def destroy
+    user = User.find(params[:user_id])
+
+    # delete stuff goes here
+
+    render json: {message_ids:params[:message_ids]},
            status: 200
   end
 end
