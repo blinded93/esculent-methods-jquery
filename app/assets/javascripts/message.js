c8600e3c4f24c927c9853b2ee98f7bc7fa10a4fd
Kyle Hughes <blinded93@gmail.com>
2018-10-17 16:56:48 -0700

    add id, createdAt and sender message attributes

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,10 @@
 function Message(json) {
+  this.id = json.id;
   this.subject = json.subject;
   this.body = json.body;
   this.readAt = json.read_at;
-
+  this.createdAt = json.created_at;
+  this.sender = json.sender;
 }
 
 
