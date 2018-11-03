b7624eed2c9a1bf0c6b230f780b835ffd770046d
Kyle Hughes <blinded93@gmail.com>
2018-11-02 18:19:48 -0700

    service class for message creation logic.

--- app/services/message_service.rb
+++ app/services/message_service.rb
@@ -1,23 +1,49 @@
 class MessageService
-  attr_accessor :messages, :scope, :user
+  attr_accessor :subject, :body, :sender_id, :recipient_id
 
-  def initialize(params, user)
-    @messages = nil
-    @scope = params[:scope] == "true" ? "unread" : params[:scope]
-    @user = user
+  def initialize(params)
+    @subject = params[:subject]
+    @body = params[:body]
+    @sender_id = params[:sender_id]
+    @recipient_id = params[:recipient_id]
   end
 
-  def filter
-    messages = self.user.send(user_scope)
-    !!message_scope ? messages.send(message_scope) : messages
+  def self.create(params, attr_method)
+    self.new(params).tap do |message|
+      message.send(attr_method).each do |attribute, value|
+        message.send(attribute) = value
+      end
+    end
   end
 
-  def user_scope
-    self.scope == "sent" ? "sent_messages" : "received_messages"
+  def self.create_and_send(type, params)
+    message_attrs = self.create(params, "#{type}_params").instance_values
+    message = Message.new(message_attrs)
   end
 
-  def message_scope
-    self.scope == "sent" || self.scope == "all" ? nil :
-    ["sent", "all"].include?(self.scope) ? nil : self.scope
-  end
+  private
+    def request_params
+      {
+        subject: "You have a friend request!",
+        user_id: self.recipient_id,
+        body: ""
+      }
+    end
+
+    def response_params
+      {
+        subject: "You have a new friend!",
+        user_id: self.sender_id,
+        body: ""
+      }
+    end
+
+    def share_params
+      {
+        subject: "#{sender} shared a recipe with you!",
+        recipe_id: recipe.id,
+        body: ""
+      }
+    end
+
 end
