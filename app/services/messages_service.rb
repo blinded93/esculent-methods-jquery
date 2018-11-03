967c7fe7e26c60a29291b55a906ca71a79d93d21
Kyle Hughes <blinded93@gmail.com>
2018-11-02 18:20:33 -0700

    service class for messages scope logic.

---
+++ app/services/messages_service.rb
@@ -0,0 +1,23 @@
+class MessagesService
+  attr_accessor :messages, :scope, :user
+
+  def initialize(params, user)
+    @messages = nil
+    @scope = params[:scope] == "true" ? "unread" : params[:scope]
+    @user = user
+  end
+
+  def filter
+    messages = self.user.send(user_scope)
+    !!message_scope ? messages.send(message_scope) : messages
+  end
+
+  def user_scope
+    self.scope == "sent" ? "sent_messages" : "received_messages"
+  end
+
+  def message_scope
+    self.scope == "sent" || self.scope == "all" ? nil :
+    ["sent", "all"].include?(self.scope) ? nil : self.scope
+  end
+end
