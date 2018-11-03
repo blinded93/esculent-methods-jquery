4e705e26d63f8c28c84a8f36b19b820548d34705
Kyle Hughes <blinded93@gmail.com>
2018-11-02 18:22:41 -0700

    add fields to message json.

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,4 +1,5 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :id, :subject, :body, :read_at, :created_at
+  attributes :id, :subject, :body, :read_at, :created_at, :user_id, :recipe_id
   belongs_to :sender
+  belongs_to :recipient
 end
