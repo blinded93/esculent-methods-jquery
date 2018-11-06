c01606feda151d941ab1d0afc0a7dff446ed01d9
Kyle Hughes <blinded93@gmail.com>
2018-11-05 17:39:21 -0800

    stub better formatted destroy method.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,25 @@
 class MessagesController < ApplicationController
   def create
-    binding.pry
+    message = current_user.sent_messages.build(message_params)
+    message.save
+    render json: message,
+           status: 200
   end
 
   def destroy
     user = User.find(params[:user_id])
-
-    # delete stuff goes here
-
-    render json: {message_ids:params[:message_ids]},
-           status: 200
+    if params[:message_ids]
+      # delete stuff goes here
+      render json: {message_ids: params[:message_ids]}, status: 200
+    elsif params[:id]
+      message = Message.find_by(id:params[:id])
+      # delete stuff here
+      render json: message, status: 200
+    end
   end
+
+  private
+    def message_params
+      params.require(:message).permit(:recipient_id, :subject, :body, :user_id, :recipe_id)
+    end
 end
