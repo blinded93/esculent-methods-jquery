f64218882baa6000103b4676ac57314d8911ee1c
Kyle Hughes <blinded93@gmail.com>
2018-11-05 17:32:15 -0800

    remove body from share/friend messages, add :user_id methods.

--- app/services/message_service.rb
+++ app/services/message_service.rb
@@ -1,5 +1,5 @@
 class MessageService
-  attr_accessor :subject, :body, :sender_id, :recipient_id
+  attr_accessor :subject, :body, :sender_id, :recipient_id, :user_id
 
   def initialize(params)
     @subject = params[:subject]
@@ -11,30 +11,28 @@ class MessageService
   def self.create(params, attr_method)
     self.new(params).tap do |message|
       message.send(attr_method).each do |attribute, value|
-        message.send(attribute) = value
+        message.send("#{attribute}=", value)
       end
     end
   end
 
   def self.create_and_send(type, params)
     message_attrs = self.create(params, "#{type}_params").instance_values
-    message = Message.new(message_attrs)
+    message = Message.create(message_attrs)
   end
 
   private
     def request_params
       {
         subject: "You have a friend request!",
-        user_id: self.recipient_id,
-        body: ""
+        user_id: self.recipient_id
       }
     end
 
     def response_params
       {
         subject: "You have a new friend!",
-        user_id: self.sender_id,
-        body: ""
+        user_id: self.sender_id
       }
     end
 
@@ -42,7 +40,6 @@ class MessageService
       {
         subject: "#{sender} shared a recipe with you!",
         recipe_id: recipe.id,
-        body: ""
       }
     end
 
