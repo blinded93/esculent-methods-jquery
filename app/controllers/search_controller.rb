6d8ea9a09d0dcf8562c8533d5681801ed6a75119
Kyle Hughes <blinded93@gmail.com>
2018-11-17 11:44:41 -0800

    remove redundant scope.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,6 @@
 #     # r =  search.valid? ? search.find_results : {search:search}
 #
 #     redirect_to recipe_search_path({params:{query:params[:query]}})
-#     binding.pry
 #     # render json: (r[:results] || r),
 #     #        meta: (r[:meta] if r[:meta]),
 #     #        status: 200
--- app/models/favorited_recipe.rb
+++ app/models/favorited_recipe.rb
@@ -1,6 +1,4 @@
 class FavoritedRecipe < ApplicationRecord
   belongs_to :user
   belongs_to :recipe
-
-  scope :for, -> (user_id) { where(user_id:user_id) }
 end
