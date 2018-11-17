d6ade42dfb37b2ddd7dfbf154dcc68d4896d8520
Kyle Hughes <blinded93@gmail.com>
2018-11-17 13:51:03 -0800

    complete service to create messages for friend request/response and recipe sharing.

--- app/services/message_service.rb
+++ app/services/message_service.rb
@@ -1,46 +1,42 @@
 class MessageService
-  attr_accessor :subject, :body, :sender_id, :recipient_id, :user_id
+  attr_accessor :subject, :body, :sender_id, :recipient_id, :user_id, :recipe_id
 
   def initialize(params)
     @subject = params[:subject]
     @body = params[:body]
     @sender_id = params[:sender_id]
     @recipient_id = params[:recipient_id]
+    @recipe_id = params[:recipe_id] if params[:recipe_id]
   end
 
-  def self.create(params, attr_method)
+  def self.create_and_send(type, params)
+    message = self.create(params, "#{type}_params")
+    message.transmit(message.instance_values)
+  end
+
+  def self.create(params, params_method)
     self.new(params).tap do |message|
-      message.send(attr_method).each do |attribute, value|
+      message.send(params_method).each do |attribute, value|
         message.send("#{attribute}=", value)
       end
     end
   end
 
-  def self.create_and_send(type, params)
-    message_attrs = self.create(params, "#{type}_params").instance_values
+  def transmit(message_attrs)
     message = Message.create(message_attrs)
   end
 
   private
     def request_params
-      {
-        subject: "You have a friend request!",
-        user_id: self.recipient_id
-      }
+      { subject: "You have a friend request!", user_id: self.recipient_id }
     end
 
     def response_params
-      {
-        subject: "You have a new friend!",
-        user_id: self.sender_id
-      }
+      { subject: "You have a new friend!", user_id: self.sender_id }
     end
 
     def share_params
-      {
-        subject: "#{sender} shared a recipe with you!",
-        recipe_id: recipe.id,
-      }
+      user = User.find_by(id:self.sender_id)
+      { subject: "#{user.username} shared a recipe with you!", recipe_id: self.recipe_id }
     end
-
 end
