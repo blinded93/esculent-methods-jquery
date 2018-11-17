ac7df1510886428e456ae7dc1549e57c45d782ad
Kyle Hughes <blinded93@gmail.com>
2018-11-17 11:47:38 -0800

    complete share model associations and scopes.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,10 +1,10 @@
 class Message < ApplicationRecord
   belongs_to :sender,
-      class_name: 'User',
-      foreign_key: 'sender_id'
+    class_name: 'User',
+    foreign_key: 'sender_id'
   belongs_to :recipient,
-      class_name: 'User',
-      foreign_key: 'recipient_id'
+    class_name: 'User',
+    foreign_key: 'recipient_id'
   belongs_to :user, optional:true
   belongs_to :recipe, optional:true
 
--- app/models/share.rb
+++ app/models/share.rb
@@ -1,2 +1,12 @@
 class Share < ApplicationRecord
+  belongs_to :user,
+    class_name: 'User',
+    foreign_key: 'user_id'
+  belongs_to :friend,
+    class_name: 'User',
+    foreign_key: 'friend_id'
+  belongs_to :recipe
+
+  scope :by_recipe, -> (id) { where(recipe_id:id) }
+  scope :by_user, -> (id) { where(user_id:id) }
 end
