3e9ffc9735778e7394de5d0b939c5730cd6bc9c6
Kyle Hughes <blinded93@gmail.com>
2018-11-17 11:04:40 -0800

    Began implementing Inbox model object.

--- app/assets/javascripts/inbox.js
+++ app/assets/javascripts/inbox.js
@@ -1,3 +1,29 @@
+function Inbox(owner) {
+  this.owner = owner;
+  this.recipients = owner.friends;
+  this.messages = owner.messages;
+}
+
+Inbox.prototype.display = function(destination) {
+  const dfd = new $.Deferred();
+  const pageObj = Paginate.createAndDestinate(this.owner.meta, destination)
+  pageObj.user = this.owner;
+  Breadcrumb.userAssets(this.owner, "Messages");
+
+  Display.fromTemplate("inbox", {friends: this.recipients})
+    .toElement(destination, "", true).done(function() {
+      Listener.setInboxBtns(user);
+      user.displayMessages("#messageInbox", pageObj)
+        .done(function() {
+          Message.deleteBtnOnCheck()    ;
+          Display.fromTemplate("pagination", pageObj)
+            .toElement("#paginationNav", 1, true).done(function() {
+              dfd.resolve(pageObj);
+            });
+        });
+    });
+  return dfd.promise();
+};
 
 Inbox.deleteMessageRows = function(resp) {
   resp.message_ids.forEach(function(id, i, arr){
