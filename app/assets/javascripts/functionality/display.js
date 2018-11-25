d908c687012bc2a5a276fd691770fcbd0b5465e9
Kyle Hughes <blinded93@gmail.com>
2018-11-25 11:14:36 -0800

    change design pattern of display model.

--- app/assets/javascripts/functionality/display.js
+++ app/assets/javascripts/functionality/display.js
@@ -1,113 +1,46 @@
-$(function() {
-  Display.homeSetup();
-  Recipe.getAllRecipes();
-});
+let display = {};
+(function() {
+  const templates = HandlebarsTemplates;
+  let templateHTML;
 
-function Display() {
-}
 
-Display.templates = HandlebarsTemplates;
+  this.fromTemplate = function(template, obj) {
+    templateHTML = templates[template](obj);
+    return this;
+  };
 
-Display.homeSetup = function() {
-  Search.setup();
-  Menu.setup();
-};
+  this.hbsPartial = function(template) {
+    return templates[template];
+  };
 
-Display.fromTemplate = function(template, obj) {
-  this.html = this.templates[template](obj);
-  return this;
-};
 
-Display.toElement = function(elementId, elapsedTime, isInbox=false) {
-  const html = this.html;
-  const dfd = new $.Deferred();
-  const t = elapsedTime || 250;
-  $(`${elementId}`).fadeOut(t, function() {
-    Display.inboxWidth(isInbox);
-    $(this).html(html).fadeIn(t);
-    dfd.resolve();
-  });
-  return dfd.promise();
-};
+  this.menuTemplate = function(template, obj) {
+    return templates[template](obj);
+  };
 
-Display.inboxWidth = function(isInbox) {
-  $main = $("#mainContent");
-  if (isInbox) {
-    $main.addClass("inbox");
-  } else {
-    $main.removeClass("inbox");
-  }
-};
 
-Display.alert = function(message, type) {
-  this.createAlert(message, type);
-  $("#alert").slideDown(200).delay(2000).slideUp(200, function() {
-    $(this).html("");
-  });
-};
+  this.toElement = function(elementId, elapsedTime, isInbox=false) {
+    const dfd = new $.Deferred();
+    const time = elapsedTime || 250;
 
-Display.createAlert = function(message, type) {
-  const div = $(`<div id="alertMessage" class="alert alert-${type} small">`).html(message);
-  $("#alert").html(div);
-  return this;
-};
+    $(`${elementId}`).fadeOut(time, function() {
+      display.width(isInbox);
+      $(this).html(templateHTML).fadeIn(time);
+      dfd.resolve();
+    });
+    return dfd.promise();
+  };
 
-Display.createSearchErrorAlert = function() {
-  this.alert("A search term is required.", "danger");
-};
 
-Display.createSearchAlert = function(query) {
-  const html = `<a href='' id='toSearchResults'>Return to results for '${query}'...</a><button id='alertDismiss' type='button' class='close'><span>&times;</span></button>`;
-  this.createAlert(html, "light");
-  Listener.setAlertDismiss("#alert", Listener.setBackToResults);
-  Listener.setAlertDismiss("#alertDismiss");
-};
+  this.width = function(isInbox) {
+    const $main = $("#mainContent");
+    toggleClass($main, "inbox", isInbox);
+    return !!isInbox;
+  };
 
-Display.createEditImageAlert = function(imgName, user) {
-  const html = `Update your image to <b>${imgName}</b>? <span class='float-right'><a href='' id='confirmImg'>Yes</a> / <a href='' id='denyImg'>No</a></span>`;
-  this.createAlert(html, "light");
-  Listener.setAlertDismiss("#confirmImg", Listener.setProfileImageSubmit(user))
-    .setAlertDismiss("#denyImg");
-  return this;
-};
 
-Display.createAddFriendAlert = function(user) {
-  const html = `Send "${user.username}" a friend request? <span class='float-right'><a href='' id='confirmFriend'>Yes</a> / <a href='' id='denyFriend'>No</a></span>`;
-  const currentUserId = $("#loggedInAs").data("id");
-
-  this.createAlert(html, "warning");
-  Display.toggleAlert();
-  Listener.setAlertDismiss("#confirmFriend", user.addFriend(currentUserId))
-    .setAlertDismiss("#denyFriend");
-  return this;
-};
-
-Display.createErrorAlert = function(message) {
-  this.alert(message, "danger");
-  return this;
-};
-
-Display.toggleAlert = function() {
-  const $a = $("#alert");
-  if ($a.is(":hidden")) {
-    $a.slideDown(200);
-    $("#mainContent").animate({ 'padding-top':35 }, 200);
-  } else {
-    $a.slideUp(200);
-    $("#mainContent").animate({'padding-top':16}, 200);
-  }
-  return this;
-};
-
-Display.alertLogIn = function() {
-  this.alert("Must be logged in to do that", "danger");
-};
-
-Display.linkSelector = function(parent) {
-  return function(child) { return $(`${parent} ${child}`) };
-};
-
-Display.nothingHere = function(destination, time, isInbox=false) {
-  Display.html = "<div class='text-center'>Nothing to show here!</div>";
-  Display.toElement(destination, time, isInbox);
-};
+  this.nothingHere = function(destination, time, isInbox=false) {
+    templateHTML = "<div class='text-center'>Nothing to show here!</div>";
+    this.toElement(destination, time, isInbox);
+  };
+}).apply(display);
