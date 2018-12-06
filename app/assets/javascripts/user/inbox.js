86ff4d7c4d41bb0669c391348107022c3e87effa
Kyle Hughes <blinded93@gmail.com>
2018-12-05 23:24:30 -0800

    add all inbox related functions to new inbox namespaced module file.

--- app/assets/javascripts/user/inbox.js
+++ app/assets/javascripts/user/inbox.js
@@ -1,36 +1,97 @@
-function Inbox(owner) {
-  this.owner = owner;
-  this.recipients = owner.friends;
-  this.messages = owner.messages;
-}
+let inbox = {};
+(function() {
+  let owner = "";
 
-Inbox.prototype.display = function(destination) {
-  const dfd = new $.Deferred();
-  const pageObj = Paginate.createAndDestinate(this.owner.meta, destination);
-  const breadcrumb = Breadcrumb.current();
-  pageObj.user = this.owner;
 
-  breadcrumb.addUserAssets(this.owner, "Messages");
-  display.fromTemplate("inbox", {friends: this.recipients})
-    .toElement(destination, "", true).done(function() {
-      Listener.setInboxBtns(user);
-      user.displayMessages("#messageInbox", pageObj)
+  this.assignOwner = function(user) {
+    owner = user;
+  };
+
+
+  this.display = function(destination) {
+    const dfd = new $.Deferred();
+    const pageObj = Paginate.createAndDestinate(this.owner.meta, destination);
+    pageObj.user = this.owner;
+
+    breadcrumb.addUserAssets(this.owner, "Messages");
+    display.fromTemplate("inbox", {friends: this.recipients})
+      .toElement(destination, "", true)
         .done(function() {
-          Message.deleteBtnOnCheck();
-          if (pageObj) { pageObj.displayLinks(dfd, destination); }
+          inbox.setBtns();
+          user.displayMessages("#messageInbox", pageObj)
+              .done(function() {
+                Message.deleteBtnOnCheck();
+                if (pageObj) { pageObj.displayLinks(dfd, destination); }
+              });
         });
-    });
-  return dfd.promise();
-};
+    return dfd.promise();
+  };
 
-Inbox.deleteMessageRows = function(resp) {
-  resp.message_ids.forEach(function(id, i, arr){
-    $(`#message-${id}`).slideUp(200, function() {
-      $(this).remove();
-      if (!$(".deleteChecks").length) {
-        display.nothingHere("#messageInbox", "", true);
-      }
-      $("#deleteBtn").fadeOut(200);
+
+  this.setBtns = function() {
+    this.deleteBtnOnCheck()
+        .setComposeBtn()
+        .setDeleteBtn()
+        .setFilterSelect();
+  };
+
+
+  this.deleteBtnOnCheck = function() {
+    const $checks = $(".deleteChecks").change(function() {
+      const checked = $checks.is(':checked');
+
+      $("#deleteBtn").stop().fadeTo(200, checked ? 1 : 0);
     });
-  });
-};
+    return this;
+  };
+
+
+  this.setComposeBtn = function() {
+    Message.setForm(owner);
+    $("#composeBtn").click(function(e) {
+      e.preventDefault();
+      $("#composeDropdown").slideToggle(200);
+    });
+    return this;
+  };
+
+
+  this.setDeleteBtn = function() {
+    const checked = $(".deleteChecks:checked");
+
+    $("#deleteBtn").click(function(e) {
+      e.preventDefault();
+      Message.deleteAll(checked);
+    });
+    return this;
+  };
+
+
+  this.setFilterSelect = function() {
+    $("#messageFilterInput").change(function(e) {
+      const selectedScope = $(this).children("option:selected").val();
+      owner.getMessages(selectedScope)
+           .done(function(data) {
+             owner.messages = data.messages;
+             user.displayMessages("#messageInbox")
+                 .done(function() {
+                   inbox.deleteBtnOnCheck();
+                 });
+           });
+    });
+    return this;
+  };
+
+
+  this.deleteMessageRows = function(resp) {
+    resp.message_ids.forEach(function(id, i, arr){
+      $(`#message-${id}`).slideUp(200, function() {
+        $(this).remove();
+        if (!$(".deleteChecks").length) {
+          display.nothingHere("#messageInbox", "", true);
+        }
+        $("#deleteBtn").fadeOut(200);
+      });
+    });
+  };
+}).apply(inbox);
