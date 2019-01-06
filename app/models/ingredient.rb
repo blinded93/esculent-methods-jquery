52a3a87063c88ffca7e34ec479dccf7a93424f7f
Kyle Hughes <blinded93@gmail.com>
2019-01-05 21:07:52 -0800

    change function to use 'like' db query using arel_table matches_any func

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -9,4 +9,10 @@ class Ingredient < ApplicationRecord
   scope :from_name, -> (name) { where(name:name.downcase).take }
   scope :id_from_name, -> (name) { from_name(name.downcase).pluck(:id) }
 
+  def self.ids_from_names(names)
+    name = Ingredient.arel_table[:name]
+    names = names.map{|v| "%#{v.downcase}%"}
+
+    Ingredient.where(name.matches_any(names)).collect(&:id)
+  end
 end
