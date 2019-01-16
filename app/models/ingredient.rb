016756dce3ac507cc36e389813462677b68c5631
Kyle Hughes <blinded93@gmail.com>
2019-01-15 23:07:42 -0800

    change methods from scope to class methods. using arel_table to search for terms

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -6,8 +6,17 @@ class Ingredient < ApplicationRecord
     joins(:ingredient_amounts).
     where(ingredient_amounts: {recipe_id: recipe_id})
   }
-  scope :from_name, -> (name) { where(name:name.downcase).take }
-  scope :id_from_name, -> (name) { from_name(name.downcase).pluck(:id) }
+
+  def self.from_name(name)
+    self.where(name:(name.downcase.singularize)).take || self.where(name:(name.downcase.pluralize)).take
+  end
+
+  def self.from_names(names)
+    name = self.arel_table[:name]
+    names = names.map{|v| "%#{v.downcase.singularize}%"}
+
+    self.where(name.matches_any(names))
+  end
 
   def self.from_like_name(n)
     name = self.arel_table[:name]
