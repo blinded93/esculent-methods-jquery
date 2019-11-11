c95fbb1ebe2811270eb493b54c5d3301149e8772
Kyle Hughes <blinded93@gmail.com>
2019-11-10 17:54:17 -0800

    move subdirectory change to assets from application.rb to assets initializer

--- config/application.rb
+++ config/application.rb
@@ -15,10 +15,6 @@ module EsculentMethods
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    Dir.glob("#{Rails.root}/app/assets/images/**/").each do |path|
-      config.assets.paths << path
-    end
-    
     config.generators do |g|
       g.test_framework :rspec
     end
